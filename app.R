56ea240bb4ccfd3b7c0dd73871c7ffa1171456e9
Nicolas Greliche <nicolas@stats4sd.org>
2021-01-08 11:55:45 +0000

    maybe we dont need devtools

--- app.R
+++ app.R
@@ -14,40 +14,264 @@ library(tidyverse)
 #library(devtools)
 library(DT)
 
+source("toConnectToKobo.R")
 # Define UI for application that draws a histogram
+
+kobohr_getdata_csv<-function(url,u,pw){
+    #supply url for the data
+    rawdata<-GET(url,authenticate(u,pw),progress())
+    d_content <- read_csv(content(rawdata,"raw",encoding = "UTF-8"), na = "n/a")
+}
+
+url_midline <-"https://kc.humanitarianresponse.info/api/v1/data/691626.csv"
+url_backcheck <-"https://kc.humanitarianresponse.info/api/v1/data/701627.csv"
+
+
+backcheck_keep <- c("index", "agency",
+                    "district_bl", "name_call_back")
+
+backcheck_var <- c("Telephone_Number",
+                   "head_hh_yn", "displacement_status",
+                   "move_plan_12mths", "MealsEatPerDay",
+                   "shelter_type", "electric_hh_yn", "HH_size", 
+                   "males_gte_18", "females_gte_18", "edu_level_hh",
+                   "meet_basic_needs", 
+                   "main_source_income", "land_owned_by","csi_score")
+backcheck_keep <- paste0(backcheck_keep,".s")
+backcheck_var <- paste0(backcheck_var,".s")
+
+midline_keep <- c("index", "agency", "State",
+                  "District", "username",
+                  "gps_location", "_gps_location_latitude", "_gps_location_longitude",
+                  "_gps_location_altitude", "_gps_location_precision", "Respondent_name", "start", "CompletionDateTime", "interviewDuration",
+                  "interviewDuringDay", "reasonableDuration", "nbDontknow")
+midline_var <- c( "PhoneNumber",
+                  "QuHeadOfHH", "DisplacementStatus_Full",
+                  "MovePlan", "MealsEatPerDay",
+                  "ShelterType", "ElectricityAccess", "hh_size", 
+                  "Male_18_plus_years_old", "Female_18_plus_years_old", "Highest_Head_Secular_Education",
+                  "SelfReliance", 
+                  "MainIncomeSource", "LandOwnership",
+                  "csi_score")
+midline_keep <- paste0(midline_keep,".m")
+midline_var <- paste0(midline_var,".m")
+varshown <- c("percentMatch", "index.m", "agency.m","username.m", "start.m", "CompletionDateTime.m","interviewDuration.m", "nbDontknow.m",
+              "State.m","District.m", c(rbind(midline_var, backcheck_var)))
+allvars <- c("percentMatch",midline_keep, midline_var, backcheck_keep, backcheck_var)
+
+listData <- function(){
+    d <- data.frame(matrix(ncol=length(varshown), nrow = 0))
+    colnames(d)<-varshown
+    return(d)
+}
+
+
+prepareData <- function(midline, backcheck){
+    
+    backcheck$csi_score <- as.numeric(backcheck$csiLess)+ as.numeric(backcheck$csiBorrow)+ as.numeric(backcheck$csiReduce)+ as.numeric(backcheck$csiAdult)+ as.numeric(backcheck$csiFewer)
+    midline$csi_score <- as.numeric(midline$csiLess)+ as.numeric(midline$csiBorrow)+ as.numeric(midline$csiReduce)+ as.numeric(midline$csiAdult)+ as.numeric(midline$csiFewer)
+    midline$interviewDuration <- difftime(midline$CompletionDateTime, midline$start, units='mins')
+    midline$interviewDuringDay <- between(format(midline$start, format="%H%M%S"),70000, 190000)
+    midline$reasonableDuration <- between(midline$interviewDuration, 30, 90)
+    midline$nbDontknow <- apply(midline,1,function(x) sum(x=="dontknow", na.rm=T))
+    
+    
+    colnames(midline) <- paste0(colnames(midline),".m")
+    colnames(backcheck) <- paste0(colnames(backcheck),".s")
+    
+    data_check <- left_join(midline[,c(midline_keep,midline_var)], backcheck[,c(backcheck_keep,backcheck_var)], by=c("index.m"="index.s"), keep=TRUE)
+    
+    data_check$qualScore <-0
+    
+    for(i in 1:length(backcheck_var)){
+        isItDifferent <- ifelse(data_check[,backcheck_var[i]]!=data_check[,midline_var[i]], 1, 0)
+        if(backcheck_var[i] %in% c("age_years", "HH_size")){
+            isItDifferent <- (abs(data_check[,backcheck_var[i]]-data_check[,midline_var[i]])) > 1
+        }else if(backcheck_var[i]=="csi_score"){
+            isItDifferent <- (abs(data_check[,backcheck_var[i]]-data_check[,midline_var[i]])) > 3
+        }
+        
+        data_check$qualScore <- data_check$qualScore + ifelse(is.na(isItDifferent), .5, isItDifferent)
+    }
+    
+    data_check$percentMatch <- ifelse(is.na(data_check$index.s), NA, 100-data_check$qualScore/length(backcheck_var)*100)
+    
+    
+    return(data_check)
+}
+
+
+
+get_data <- function(login, password){
+    d_midline <- tryCatch(kobohr_getdata_csv(url_midline,login,password), error=function(e){message("can't access data")})
+    d_backcheck <- tryCatch(kobohr_getdata_csv(url_backcheck,login,password), error=function(e){message("can't access data")})
+    if(length(d_midline)>1 & length(d_midline)>1){
+        midline <- as.data.frame(d_midline)
+        colnames(midline) <-gsub(".*/","",colnames(midline))
+        midline$agency[is.na(midline$agency)] <- "-"
+        midline$username[is.na(midline$username)] <- "-"
+        backcheck <- as.data.frame(d_backcheck)
+        colnames(backcheck) <-gsub(".*/","",colnames(backcheck))
+        return(prepareData(midline, backcheck))
+    }
+}
+
+library(shiny)
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    
+    titlePanel("LORI data collection quality control"),
+    
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 20,
-                        value = 30)
+            actionButton("load_data", "Load data"),
+            #selectInput("summary_by", "Choose focus of quality summary table", c("agency.m", "username.m"), multiple = TRUE),
+            pickerInput("summary_by", "Summary by (top table)", c("agency.m", "username.m"), selected = c("agency.m"), multiple = TRUE),
+            pickerInput("filter_agency", "Filter agency partner",sort(unique(listData()$agency.m), na.last=TRUE),selected = unique(listData()$agency.m),options = list(`actions-box` = TRUE), multiple = T),
+            pickerInput("filter_username", "Filter username",sort(unique(listData()$username.m), na.last=TRUE),selected=unique(listData()$username.m),options = list(`actions-box` = TRUE), multiple = T),
+            h5("For bottom table:"),
+            sliderInput("dontknow_threshold",
+                        "Show when nomber of dont know is greater than... ",
+                        min=0,
+                        max=50,
+                        value=0),
+            #sliderInput("duration_threshold",
+            #            "Show when interview durations is less than... ",
+            #            min=0,
+            #            max=999900,
+            #            value=200),
+            sliderInput("data_quality_threshold",
+                        "Show when back-check percent match is less than... ",
+                        min=0,
+                        max=100,
+                        value=90),
+            downloadButton("downloadtable1", "Download top table"),
+            downloadButton("downloadtable2", "Download bottom table")
         ),
-
-        # Show a plot of the generated distribution
+        
         mainPanel(
-           plotOutput("distPlot")
+            dataTableOutput("summary_table"),
+            br(),br(),
+            dataTableOutput("data")
         )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+server <- function(input, output, session) {
+    data <- reactiveValues()
+    data$check <- data.frame(matrix(ncol=length(allvars), nrow = 0, dimnames=list(NULL, allvars)) )
+    # observe({showModal(modalDialog(
+    #   title = "start",
+    #   paste(colnames(data$check)[1:5], collapse=","),
+    #   easyClose = TRUE,
+    #   footer = NULL
+    # ))
+    # })
+    observeEvent(input$load_data, {
+        showModal(modalDialog(
+            textInput('login', 'Please enter login to access data'),
+            textInput('password', 'Please enter password to access data'),
+            footer=tagList(
+                actionButton('submit', 'Submit'),
+                modalButton('cancel')
+            )
+        ))
     })
+    observeEvent(input$submit, {
+        data$check <- get_data(isolate(input$login), isolate(input$password))
+        removeModal()
+        updatePickerInput(session, "filter_agency", choices = sort(unique((data$check)$agency.m), na.last=TRUE),selected = unique((data$check)$agency.m))
+    })
+    
+    
+    # update list of usernames
+    updatedChoices = reactive({
+        filtered_data <- isolate(data$check) %>%
+            filter(agency.m %in% input$filter_agency)
+        enum_choices <- filtered_data %>%
+            pull(username.m) %>% unique() %>% sort(na.last=TRUE)
+        tmp<-list(enum_choices)
+        return(tmp)
+    })
+    observe({
+        updatePickerInput(session, "filter_username", choices = updatedChoices()[[1]], selected=updatedChoices()[[1]])
+    })
+    
+    
+    
+    
+    # Prepare top table (summary)
+    summaryTable <- reactive({
+        # showModal(modalDialog(
+        #      title = "start",
+        #      paste(colnames(data$check)[1:5], collapse=","),
+        #      easyClose = TRUE,
+        #      footer = NULL
+        #    ))
+        isolate(data$check) %>%
+            filter(agency.m%in%input$filter_agency,
+                   username.m %in% input$filter_username)%>%
+            group_by_at(vars(input$summary_by))%>%
+            summarise(N=sum(!is.na(index.m), na.rm=T),
+                      time_ok=mean(interviewDuringDay.m, na.rm=TRUE),
+                      duration_ok = mean(reasonableDuration.m, na.rm=TRUE),
+                      avg_dontknow = mean(nbDontknow.m),
+                      N_backchecked=sum(!is.na(index.s), na.rm=T),
+                      #prop_bc=sum(!is.na(index.s), na.rm=T)/sum(!is.na(index.m), na.rm=T),
+                      avg_match_perc=mean(percentMatch, na.rm=T)/100,
+                      min_match_perc=min(percentMatch, na.rm=T)/100,
+                      #`25percMatch`=quantile(percentMatch, probs = .25, na.rm=T)/100
+                      
+            )
+    })
+    
+    # Prepare bottom table
+    filteredRawData <- reactive({
+        data$check %>%
+            
+            # Apply filter
+            #filter(!is.na(index.s))%>%
+            filter(percentMatch<=input$data_quality_threshold | input$data_quality_threshold==100)%>%
+            filter(nbDontknow.m>=input$dontknow_threshold)%>%
+            #filter(reasonableDuration.m<=input$duration_threshold | is.na(input$duration_threshold))%>%
+            
+            filter(username.m %in% input$filter_username,
+                   agency.m%in%input$filter_agency)%>%
+            .[,varshown]
+    })
+    
+    
+    # show the top table
+    output$summary_table <- renderDataTable({
+        datatable(summaryTable()) %>%
+            formatPercentage(c("avg_match_perc", "min_match_perc", "duration_ok", "time_ok"), 0)%>%
+            #formatRound(c("prop"), 3)%>%
+            formatRound(c("avg_dontknow"), 1)
+    })
+    
+    # show the bottom table
+    output$data <- renderDataTable({
+        datatable(filteredRawData()) %>%
+            formatRound(c("percentMatch"), 2)%>%
+            formatRound(c("interviewDuration.m"), 0)
+    })
+    
+    # make the download top table button
+    output$downloadtable1 <- downloadHandler(
+        filename = function() "summaryTable.csv",
+        content = function(file) {
+            write.csv(summaryTable(), file, row.names = FALSE)
+        }
+    )
+    
+    # make the download bottom table button
+    output$downloadtable2 <- downloadHandler(
+        filename = function() "filteredRawData.csv",
+        content = function(file) {
+            write.csv(filteredRawData(), file, row.names = FALSE)
+        }
+    )
 }
 
 # Run the application 
---
+++ toConnectToKobo.R
@@ -0,0 +1,351 @@
+'-
+***********************************************
+Developed by: Punya Prasad Sapkota
+Last Modified: 23 May 2018
+***********************************************
+#---USAGE
+#--TWO Sections
+#Section 1
+#-----KoBo data Access using API v1
+#-----https://kc.humanitarianresponse.info/api/v1
+#Section 2
+#-----new KoBo KPI
+-'
+
+###--------SECTION 2---KPI---------
+#---Upload form in NEW KoBo toolbox--------
+#STEP1------
+#Import your form first (POST to https://kobo.humanitarianresponse.info/imports/)
+#which will create a new asset in KPI which you can then deploy 
+#by POSTing to https://kobo.humanitarianresponse.info/assets/[asset ID number]/deployment/
+#USAGE: kobo_formxlsx="./xlsform/kobo_1701_NW.xlsx"
+#url = "https://kobo.humanitarianresponse.info/imports/"
+
+#POST a new import as a multipart form. The necessary parameters are library=false and file, which cotains the XLSForm:
+#john@scrappy:/tmp/api_demo$ curl --silent --user jnm_api:test-for-punya --header 'Accept: application/json' 
+#-X POST https://kobo.humanitarianresponse.info/imports/ --form library=false --form file=@kobo_1701_NW.xlsx | python -m json.tool
+
+kobohr_kpi_upload_xlsform <-function(url,kobo_form_xlsx,u,pw){
+  #STEP 1 ---importing a form---
+  #POST a new import as a multipart form. The necessary parameters are library=false and file, which cotains the XLSForm:
+  #john@scrappy:/tmp/api_demo$ curl --silent --user jnm_api:test-for-punya --header 'Accept: application/json' 
+  #-X POST https://kobo.humanitarianresponse.info/imports/ --form library=false --form file=@kobo_1701_NW.xlsx | python -m json.tool
+  result<-httr::POST(url=url,
+                     body=list(
+                       file=upload_file(path=kobo_form_xlsx),
+                       library = 'false'
+                     ),
+                     authenticate(u,pw)
+  )
+  #return asset uid
+  d_content <- rawToChar(result$content)
+  d_content <- fromJSON(d_content)
+  #get the url to pass to the next step
+  status<- d_content$status ##--success - status = 'processing'
+  print (status)
+  import_url<-d_content$url
+  print (paste0("Asset Import URL - ",import_url))
+  return(d_content) 
+}
+
+#STEP2----Get the asset UID-------------------------
+#The asset UID is given by messages.created[0].uid in the JSON response when GETting an import. 
+#Multiple GETs may be required until the server indicates "status": "complete" in the response:
+#john@scrappy:/tmp/api_demo$ curl --silent --user jnm_api:test-for-punya --header 'Accept: application/json' 
+#https://kobo.humanitarianresponse.info/imports/iGYukk6NVEA64zwMsPtgRD/ | python -m json.tool
+# the URL is fetched from the output of 
+
+kobohr_kpi_get_asset_uid<-function(url, u, pw){
+  result<-GET(url=url,authenticate(u,pw),progress())
+  d_content <- rawToChar(result$content)
+  d_content <- fromJSON(d_content)
+  #--success - status = 'complete'
+  print (d_content$status)
+  asset_uid <- d_content$messages$created$uid
+  print (paste0("Asset UID - ", asset_uid))
+  #print (paste0("Asset UID - ", asset_uid))
+  return(d_content)
+}
+
+#STEP3-----Deploying an Asset------
+#Deploying an asset
+#Construct a URL using the asset UID: https://kobo.humanitarianresponse.info/assets/[your-asset-UID]/deployment/; 
+#then, POST with the form parameter active=true.
+#john@scrappy:/tmp/api_demo$ curl --silent --user jnm_api:test-for-punya --header 'Accept: application/json' 
+#-X POST https://kobo.humanitarianresponse.info/assets/apLBsTJ4JAReiAWQQQBKNZ/deployment/ --form active=true | python -m json.tool
+
+#d<-list(owner=paste0("https://kobo.humanitarianresponse.info/users/",kobo_user,"/"),active=TRUE)
+#kobo_server_url<-""https://kobo.humanitarianresponse.info/"
+kobohr_kpi_deploy_asset<- function (asset_uid, u, pw){
+  asset_deployment_url <-paste0(kobo_server_url,"assets/",asset_uid,"/deployment/")
+  d <- list(owner=paste0(kobo_server_url, "users/",u,"/"),active=TRUE)
+  result<-httr::POST (url=asset_deployment_url,
+                      body=d,
+                      authenticate(u,pw)
+  )
+  d_content <- rawToChar(result$content)
+  d_content <- fromJSON(d_content)
+  print (paste0("Deployment Success - ", d_content$identifier))
+  return(d_content)
+}
+
+# #Share asset with another user
+# # Share a form
+# # Sharing requires a POST to "https://kobo.humanitarianresponse.info/permissions/"
+# # with three form parameters
+# # content_object: the URL, relative or absolute, of the asset to be shared, e.g. /assets/aSAKqcFRv9nYWqiKGvZC7w/
+# # permission: a string indicating the permission to grant, e.g. change_asset. This can be any assignable permission for an
+# # asset
+# # user: the URL, relative or absolute, identifying the user who will receive the new permission, e.g. /users/jnm/. The last
+# # component of the URL is the username.
+# # john@scrappy:/tmp/api_demo$ curl --silent --user jnm_api:test-for-punya --header 'Accept:
+# # application/json' -X 
+# # POST "https://kobo.humanitarianresponse.info/permissions/" --form
+# # content_object=/assets/apLBsTJ4JAReiAWQQQBKNZ/ --form permission=change_asset --form
+# # user=/users/jnm/ | python -m json.tool
+# ##SHARE AssET
+# # Assignable permissions that are stored in the database
+# # ASSIGNABLE_PERMISSIONS = (
+# #   'view_asset',
+# #   'change_asset',
+# #   'add_submissions',
+# #   'view_submissions',
+# #   'change_submissions',
+# #   'validate_submissions',
+# # )
+# 
+# #kobo_server_url<-"https://kobo.humanitarianresponse.info/"
+
+kobohr_kpi_share_asset<-function(content_object_i, permission_i, user_i, u, pw){
+  asset_share_url <-paste0(kobo_server_url,"permissions/")
+  d <- list(content_object=content_object_i, permission=permission_i, user=user_i)
+  result<-httr::POST (url=asset_share_url,
+                      body=d,
+                      authenticate(u,pw)
+  )
+  d_content <- rawToChar(result$content)
+  d_content <- fromJSON(d_content)
+  return(d_content)
+}
+
+#----CREATE EXPORTS-------------------------#
+#Examples to call functions
+# d_exports<-kobohr_create_export(type=type,lang=lang,fields_from_all_versions=fields_from_all_versions,hierarchy_in_labels=hierarchy_in_labels,group_sep=group_sep,asset_uid=asset_uid,kobo_user,Kobo_pw)
+# d_exports<-as.data.frame(d_exports)
+# 
+# d_latest_export<-kobohr_latest_exports(asset_uid,kobo_user,Kobo_pw)
+# d_latest_export<-as.data.frame(d_latest_export)
+#################-----https://github.com/tinok/kobo_api/blob/master/get_csv.py--------------#########
+# ###Get asset information
+# asset_uid<-"aaUR7DuXPLGsVUqMbv7BcB"
+# ###define some variables
+# # These variables define the default export. Each value can be overwritten when running the `create' command
+# # e.g. `create xml 'English (en_US)' true'. It's possible to skip some arguments at the end but they need to be passed in the same order.
+# asset_uid_<-asset_uid
+# type <- "csv"
+# lang <- "xml"
+# fields_from_all_versions <- "true"
+# hierarchy_in_labels <- "false"
+# group_sep = "/"
+# set kobo server URL
+# KoBo server URL is passed as a global variable
+# kobo_server_url<-"https://kobo.humanitarianresponse.info/"
+# kc_server_url<-"https://kc.humanitarianresponse.info/"
+
+
+###--CREATE EXPORT-----------
+kobohr_create_export<-function(type,lang,fields_from_all_versions,hierarchy_in_labels,group_sep,asset_uid,kobo_user,Kobo_pw){
+  api_url_export<-paste0(kobo_server_url,"exports/")
+  api_url_asset<-paste0(kobo_server_url,"assets/",asset_uid,"/")
+  api_url_export_asset<-paste0(kobo_server_url,"exports/",asset_uid,"/")
+  #
+  d<-list(source=api_url_asset,
+          type=type,
+          lang=lang,
+          fields_from_all_versions=fields_from_all_versions,
+          hierarchy_in_labels=hierarchy_in_labels,
+          group_sep=group_sep)
+  #fetch data
+  result<-httr::POST (url=api_url_export,
+                      body=d,
+                      authenticate(kobo_user,Kobo_pw),
+                      progress()
+  )
+  
+  print(paste0("status code:",result$status_code))
+  d_content <- rawToChar(result$content)
+  print(d_content)
+  d_content <- fromJSON(d_content)
+  return(d_content)
+}
+
+###---list exports----------------###
+#https://www.r-bloggers.com/using-the-httr-package-to-retrieve-data-from-apis-in-r/
+kobohr_list_exports<-function(asset_uid,kobo_user,Kobo_pw){
+  api_url_export<-paste0(kobo_server_url,"exports/")
+  api_url_asset<-paste0(kobo_server_url,"assets/",asset_uid,"/")
+  api_url_export_asset<-paste0(kobo_server_url,"exports/",asset_uid,"/")
+  #
+  payload<-list(q=paste0('source:',asset_uid))
+  #payload<-list(source=asset_uid_)
+  #fetch data
+  result<-httr::GET (url=api_url_export,
+                     query=payload,
+                     authenticate(kobo_user,Kobo_pw),
+                     progress()
+  )
+  
+  warn_for_status(result)
+  stop_for_status(result)
+  
+  print(paste0("status code:",result$status_code))
+  #
+  d_content <- rawToChar(result$content)
+  Encoding(d_content) <- "UTF-8"
+  d_content <- fromJSON(d_content)
+  #
+  #d_content<-fromJSON(content(result,"text",encoding = "UTF-8"))
+  
+  #get both result and data
+  d_content_result<-data.frame(d_content$results$result,d_content$results$date_created,d_content$results$last_submission_time)
+  #replace texts in the field name
+  names(d_content_result)<-str_remove(names(d_content_result),"d_content.results.")
+  d_content_data<-data.frame(d_content$results$data)
+  d_content_all<-bind_cols(d_content_result,d_content_data)
+  ##rename
+  #names(d_content_all)<-c("result",names(d_content_data))
+  #filter content by the asset uid
+  d_content_list<-d_content_all %>% 
+    filter(str_detect(source,asset_uid))
+  #d_content_ <-as.data.frame(d_content$results$url,d_content$results$uid)
+  return(d_content_list)
+}
+
+#Get the latest export URL
+kobohr_latest_exports<-function(asset_uid,kobo_user,Kobo_pw){
+  d_list_export_urls<-kobohr_list_exports(asset_uid,kobo_user,Kobo_pw)
+  d_list_url<-d_list_export_urls[nrow(d_list_export_urls),]
+  #latest_url<-d_list_url$result
+  latest_url<-d_list_url
+  return(latest_url)
+}
+
+# Download Asset list - form list
+kobohr_kpi_get_asset_list<-function(kobo_server_url, kobo_user,Kobo_pw){
+  api_url <-paste0(kobo_server_url, "forms/assets/?limit=1000&offset=0")
+  result<-GET(api_url,authenticate(kobo_user,Kobo_pw),progress())
+  ##print (result)
+  #
+  d_content <- rawToChar(result$content)
+  Encoding(d_content) <- "UTF-8"
+  #
+  d_content <- fromJSON(d_content)
+  d_asset<-data.frame(d_content$results$url,
+                      d_content$results$date_modified,
+                      d_content$results$owner,
+                      d_content$results$owner__username,
+                      d_content$results$uid,
+                      d_content$results$kind,
+                      d_content$results$name,
+                      d_content$results$asset_type,
+                      d_content$results$deployment__active,
+                      d_content$results$deployment__identifier,
+                      d_content$results$deployment__submission_count)
+  
+  names(d_asset)<-c("url","date_modified","owner","owner__username","uid","kind","name","asset_type","deployment__active","deployment__identifier","deployment__submission_count")
+  return (d_asset)
+}
+
+
+###################--------SECTION 1---########################################
+#########--------------------KC--------------------############################
+#---Upload form in KoBo toolbox--------
+#curl -X POST -F xls_file=@/path/to/form.xls https://kobo.humanitarianresponse.info/api/v1/forms
+#POST(url, body = upload_file("mypath.txt"))
+#USAGE: kobo_formxlsx="./xlsform/kobo_1701_NW.xlsx"
+#url = "https://kc.humanitarianresponse.info/api/v1/forms"
+# kobohr_upload_xlsform <-function(url,kobo_xlsform,u,pw){
+#   result<-httr::POST (url,
+#                       body=list(
+#                         xls_file=upload_file(path=kobo_xlsform)),
+#                       authenticate(u,pw))
+#   result<-result
+# }
+
+#user names and password to be loaded from external authenticate file - this approach to be checked
+#returns list of forms as a dataframe
+#url <- "https://kc.humanitarianresponse.info/api/v1/data.csv"
+kobohr_getforms_csv <-function(url,u,pw){
+  #supply url
+  rawdata<-GET(url,authenticate(u,pw),progress())
+  d_content_csv <-read_csv(content(rawdata,"raw",encoding = "UTF-8"))
+}
+
+#returns the CSV content of the form
+#url<-https://kc.humanitarianresponse.info/api/v1/data/145448.csv
+kobohr_getdata_csv<-function(url,u,pw){
+  #supply url for the data
+  rawdata<-GET(url,authenticate(u,pw),progress())
+  d_content <- read_csv(content(rawdata,"raw",encoding = "UTF-8"))
+}
+
+#submission count
+#returns number of data submisstion in each form
+#url<-'https://kc.humanitarianresponse.info/api/v1/stats/submissions/145533?group=anygroupname'
+kobohr_count_submission <-function(url,u,pw){
+  #supply url
+  rawdata<-GET(url,authenticate(u,pw),progress())
+  d_content <- rawToChar(rawdata$content)
+  d_content <- fromJSON(d_content)
+  d_count_submission <- d_content$count
+  #check whether there is record or not
+  if (is.null(d_count_subm)){
+    d_count_submission <-0
+  }
+  return(d_count_submission)
+}
+
+#downlod data in CSV format
+#---Parameters
+#-----formid <- 145533
+#-----savepath <- "./data/data_export_csv/"
+#-----u <- "username"
+#-----pw <- "password"
+# set kobo server URL
+#kobo_server_url<-"https://kobo.humanitarianresponse.info/"
+#kc_server_url<-"https://kc.humanitarianresponse.info/"
+
+
+kobohr_download_csv<-function(formid,savepath,u,pw){
+  #check the submission first
+  d_count_subm<-NULL
+  stat_url<- paste0(kc_server_url,"api/v1/stats/submissions/",formid,"?group=anygroup")
+  d_count_subm <- kobohr_count_submission (stat_url,u,pw)
+  #download data only if submission
+  if (!is.null(d_count_subm)){
+    #Example "https://kc.humanitarianresponse.info/api/v1/data/79489.csv"
+    kobo_csv_url <- paste0(kc_server_url,"api/v1/data/", formid,".csv")
+    d_rawi<-kobohr_getdata_csv(kobo_csv_url,u,pw)
+    #write to csv
+    #save file name
+    savefile <- paste0(savepath,formid,"_data.csv")
+    write_csv(d_rawi,savefile)
+  }
+}
+
+#------------------------------------------------------------#
+#supply url
+#user names and password to be loaded from external authenticate file - this approach to be checked
+kobohr_getforms <-function(url,u,pw){
+  #supply url
+  rawdata<-GET(url,authenticate(u,pw),progress())
+  d_content <- rawToChar(rawdata$content)
+  d_content <- fromJSON(d_content)
+}
+
+kobohr_getdata<-function(url,u,pw){
+  #supply url for the data
+  rawdata<-GET(url,authenticate(u,pw),progress())
+  d_content <- rawToChar(rawdata$content)
+  d_content <- fromJSON(d_content)
+}
